7d8b5cb859f9cf522af4932d3dd73619cbe358e0
kevinle108 <kevinle108@gmail.com>
2020-11-13 20:34:51 -0500

    add Vote class and methods

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Tabulator
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Tabulator Program");
+            string lineOfText = "1,00000400,Kate Stewart]Miley Houston]David Michael,Miley Houston]David Michael,Roger Schlegel,";
+
+            Vote test = new Vote(lineOfText);
+            test.Display();
+
+            Console.WriteLine($"1st Choice: {test.FirstChoice()}");
+
+            Console.WriteLine();
+            Console.WriteLine("Testing input file:");
+            Console.WriteLine();
+            StreamReader file = new StreamReader("Mayor.csv");
+            string line = file.ReadLine();
+            Console.WriteLine(line);
+            Vote testVote = new Vote(line);
+            testVote.Display();
+
+            Console.WriteLine();
+            Console.WriteLine("Eliminate David Michael:");
+            testVote = testVote.Eliminate("David Michael");
+            testVote.Display();
+
+            Console.WriteLine();
+            Console.WriteLine("Is vote exhausted? ...");
+            if (testVote.IsExhausted() == true)
+            {
+                Console.WriteLine("Vote is exhausted! Remove this vote.");
+            }
+            else if (testVote.IsExhausted() == false)
+            {
+                Console.WriteLine("Vote is still good!");
+            }
+
+
+
+
+
+
         }
+
+
     }
 }
---
+++ Vote.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Tabulator
+{
+    class Vote
+    {
+        // a vote is a List<List<String>>
+        List<List<String>> Item = new List<List<String>>();
+        readonly char NAME_SEPARATOR = ']';
+        public Vote(string lineOfText)
+        {
+            List<List<string>> vote = new List<List<string>>(); // create an empty nested list
+
+            // uncomment to allow names to contain commas and double quotes
+            Regex regx = new Regex(',' + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+            var postSplit = regx.Split(lineOfText).Skip(2);
+            postSplit = postSplit.Select(x => x.Trim('"'));
+
+            // uncomment to not allow commas and double quotes in names
+            //var postSplit = lineOfText.Split(',').Skip(2); // ignores first 2 elements
+
+            // if the lineOfText ends with a comma, then last element will be an empty string ""
+            // remove this last empty string from the list
+            if (postSplit.Last() == "")
+            {
+                postSplit = postSplit.SkipLast(1).ToList();
+            }
+            
+
+            foreach (string ele in postSplit)
+            {
+                if (ele.Contains(NAME_SEPARATOR))
+                {
+                    vote.Add(ele.Split(']').ToList());
+                }
+                else
+                {
+                    vote.Add(new List<string> { ele });
+                }
+            }
+
+            Item = vote;
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < Item.Count; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Vote.Item[{i}]");
+                for (int j = 0; j < Item[i].Count; j++)
+                {
+                    Console.WriteLine(Item[i][j]);
+                }
+            }
+        }
+
+        public string FirstChoice()
+        {
+            // use Aggregate() in case there is more than 1 candidate for 1st Choice
+            return Item[0].Aggregate((message, name) => $"{message} & {name}");
+        }
+
+        public Vote Eliminate(string name)
+        {
+            for (int i = 0; i < Item.Count; i++)
+            {
+                Item[i].RemoveAll(x => x == name);
+            }
+
+            // clean up choices that have null candidates
+            Item.RemoveAll(x => x.Count == 0);
+
+            return this;
+        }
+
+        public bool IsExhausted()
+        {
+            return Item[0].Count > 1;
+        }
+
+        
+
+    }
+}
