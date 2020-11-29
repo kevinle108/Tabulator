f7ef28e21fe773505d113d525559bcd07840d1a9
Kevin Le <kevinle108@gmail.com>
2020-11-29 18:48:32 -0500

    add ask name for elimination

--- Round.cs
+++ Round.cs
@@ -35,6 +35,29 @@ namespace Tabulator
         public List<string> NamesToEliminate(List<Round> pastRounds)
         {
             List<string> names = new List<string>();
+            if (Tally.Select(x => x.Count).Distinct().ToList().Count == 1) // there is a tie
+            {
+                int tiedCount = Tally[0].Count;
+                List<string> namesTied = Tally.Where(x => x.Count == tiedCount).Select(x => x.Name).ToList();
+                Console.WriteLine($"\n\rThere are {namesTied.Count} candidates tied with {tiedCount} votes each:");
+                foreach (string name in namesTied)
+                {
+                    Console.WriteLine(name);
+                }
+                bool askName = true;
+                while (askName)
+                {
+                    Console.Write("\n\rEnter the name of the candidate to eliminate: ");
+                    string inputName = Console.ReadLine();
+                    if (namesTied.Contains(inputName))
+                    {
+                        askName = false;
+                        names.Add(inputName);
+                        return names;
+                    }
+                }
+            }
+            
             int savedIndex = 0;
             int needed = Tally.Sum(x => x.Count) / 2;
             for (int i = Tally.Count-1; i > 0; i--)
@@ -44,21 +67,24 @@ namespace Tabulator
                     savedIndex = i;
                 }
             }
-            if (savedIndex == 0)
+            if (savedIndex == 0) // if tied, use pastrounds for tiebreaker
             {
-                string name1 = Tally[savedIndex].Name;
-                Round lastRound = pastRounds[pastRounds.Count - 1];
-                int name1Count = lastRound.Tally[savedIndex].Count;
-                int name2Count = lastRound.Tally[savedIndex+1].Count;
-                if (name1Count > name2Count)
+                if (pastRounds.Count > 0)
                 {
-                    names.Add(Tally[savedIndex + 1].Name);
+                    string name1 = Tally[savedIndex].Name;
+                    Round lastRound = pastRounds[pastRounds.Count - 1];
+                    int name1Count = lastRound.Tally[savedIndex].Count;
+                    int name2Count = lastRound.Tally[savedIndex + 1].Count;
+                    if (name1Count > name2Count)
+                    {
+                        names.Add(Tally[savedIndex + 1].Name);
+                    }
+                    else
+                    {
+                        names.Add(Tally[savedIndex].Name);
+                    }
+                    return names;
                 }
-                else
-                {
-                    names.Add(Tally[savedIndex].Name);
-                }
-                return names;
             }
             if (Tally[savedIndex].Count == Tally[savedIndex-1].Count)
             {
